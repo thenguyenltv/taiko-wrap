0c31f0741610ce95d8d0f0962df2179d34162dae
_thenguyen <thenguyenltv@gmail.com>
2025-04-21 13:47:12 +0700

    Update: setup the .bat file to run process

--- wrap.bat
+++ wrap.bat
@@ -2,13 +2,13 @@
 
 set KEY=YOUR_KEY
 set RPC_URL=YOUR_RPC
-node wrap.js 
+node wrap.js [TARGET_POINT_VOLUME] [TARGET_POINT_TNX]
 
 pause
 
 :: ___Example___
 :: @echo off
-:: set KEY=0x123456ReplaceYourPrivateKeyHere
+:: set KEY=0x1234567890abcdef1234567890abcdef12345678
 :: set RPC_URL=https://rpc.mainnet.taiko.xyz
-:: node wrap.js
+:: node wrap.js 73580 73580
 :: pause
